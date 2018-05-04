1ffc599c12cc82f25ce6b4ce2f9547971b4c9666
Murugar Subram <davanon2014@gmail.com>
2018-05-04 12:04:09 +0530

    Update MainController.java

--- src/main/java/com/iqmsoft/MainController.java
+++ src/main/java/com/iqmsoft/MainController.java
@@ -14,6 +14,6 @@ public final class MainController {
     @GetMapping("/")
     public String index() {
         logger.info("index");
-        return "test";
+        return "this is a sample test";
     }
 }
